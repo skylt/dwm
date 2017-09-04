41e286f30cf6301b023d3fbc1df17bb56e4f21af
Brendan Harley <harley_b@epita.fr>
2017-09-04 16:13:41 +0200

    Update with personnal conf

--- config.h
+++ config.h
@@ -34,7 +34,11 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
+<<<<<<< HEAD
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+=======
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+>>>>>>> Update with personnal conf
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
