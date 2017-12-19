e968d4c7f47c65193f3903f30178a4956ea3fa39
Brendan Harley <brendan@lse.epita.fr>
2017-12-19 06:21:19 +0100

    Update
    Signed-off-by: Brendan Harley <brendan@lse.epita.fr>

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=8" };
+static const char *fonts[]          = { "monospace:size=7" };
 static const char dmenufont[]       = "monospace:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -34,11 +34,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-<<<<<<< HEAD
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-=======
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
->>>>>>> Update with personnal conf
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -75,7 +71,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_o,      incnmaster,     {0} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
