ac9aa7b3b13d4c509e4ade97e839dcb85d4adb07
Brendan Harley <brendan@lse.epita.fr>
2018-08-16 12:09:36 +0200

    Add sleep and lock key
    Signed-off-by: Brendan Harley <brendan@lse.epita.fr>

--- config.h
+++ config.h
@@ -60,11 +60,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "uxterm", NULL };
 static const char *lockcmd[]  = { "bash", "/home/skylt/lock", NULL };
+static const char *locksleepcmd[]  = { "bash", "/home/skylt/locksleep", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_z,      spawn,          {.v = lockcmd } },
+	{ MODKEY|ControlMask,           XK_z,      spawn,          {.v = locksleepcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
