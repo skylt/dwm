b023c7bde9b5f37030cb649db36da9a6859eafa8
Brendan Harley <bharley@splio.com>
2021-06-07 11:06:01 +0200

    config.h: add backgroundcmd
    Signed-off-by: Brendan Harley <bharley@splio.com>

--- config.h
+++ config.h
@@ -62,12 +62,14 @@ static const char *termcmd[]  = { "uxterm", NULL };
 //static const char *termcmd[]  = { "uxterm", "-fg", "black", "-bg", "white", NULL };
 static const char *lockcmd[]  = { "bash", "/home/skylt/lock", NULL };
 static const char *locksleepcmd[]  = { "bash", "/home/skylt/locksleep", NULL };
+static const char *backgroundcmd[]  = { "bash", "/home/skylt/.fehbg", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_z,      spawn,          {.v = lockcmd } },
 	{ MODKEY|ControlMask,           XK_z,      spawn,          {.v = locksleepcmd } },
+	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = backgroundcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
